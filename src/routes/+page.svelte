57e1d8c75e8ed84396d5ea0db7a64849aa8989b3
Doug A <douganderson444@gmail.com>
2023-06-19 17:08:38 -0300

    demo plugin use, easy peasy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,88 @@
 <script>
 	import { plugin } from '$lib';
-	import { onMount, tick } from 'svelte';
-
+	import { onMount } from 'svelte';
 	import { rollup } from '@rollup/browser';
 
-	const modules = {
-		'main.js': "import foo from 'foo.js'; export {foo};",
-		'foo.js': 'export default () => console.log(42);'
-	};
+	import { generate } from '$lib/bindgenComp/js-component-bindgen-component.js';
+	// @ts-ignore
+	import wasm from './hello.world.comp.wasm?url';
 
-	let bundled;
 	/**
-	 * @type {BlobPart}
+	 * @type {string | null}
+	 */
+	let whatSayYou;
+	/**
+	 * @type {string}
 	 */
 	let code;
 
 	onMount(async () => {
+		// get wasm bytes from url
+		let wasmBytes = await fetch(wasm).then((res) => res.arrayBuffer());
+
+		let map = Object.assign(
+			{
+				'wasi:*': 'https://unpkg.com/@bytecodealliance/preview2-shim@0.0.9/lib/browser/*'
+			},
+			{
+				// specify location of imported functions, if applicable
+				// 'component:cargo-comp': `http://localhost:5173/ipns-pubsub.js`
+			}
+		);
+
+		// see jco/cmd/transpile.js
+		let opts = {
+			name: 'hello-world',
+			map: Object.entries(map ?? {}),
+			instantiation: false,
+			validLiftingOptimization: false,
+			noNodejsCompat: true,
+			tlaCompat: false,
+			base64Cutoff: 999999
+		};
+		// pass into generate along with bytes
+		let { files, imports, exports } = generate(wasmBytes, opts);
+
+		console.log({ files, imports, exports });
+
+		// go through files, imports, and exports arrays and take element[0] make it the key, and element[1] the value
+		// then pass that into rollup
+		const decoder = new TextDecoder();
+		let modules = Object.fromEntries(
+			files.map((/** @type {any[]} */ element) => {
+				if (element[0].endsWith('.js') || element[0].endsWith('.ts')) {
+					return [element[0], decoder.decode(element[1])];
+				} else {
+					return [element[0], element[1]];
+				}
+			})
+		);
+
 		code = await rollup({
-			input: 'main.js',
+			input: 'hello-world.js',
 			plugins: [plugin(modules)]
 		})
 			.then((bundle) => bundle.generate({ format: 'es' }))
 			.then(({ output }) => output[0].code);
 
-		console.log({ code });
-
-		// generate url from blob
+		// generate url from code blob
 		let blob = new Blob([code], { type: 'text/javascript' });
 		let url = URL.createObjectURL(blob);
 
-		let app = await import(/* @vite-ignore */ url);
+		let { helloWorld } = await import(/* @vite-ignore */ url);
 
-		console.log({ app });
-		app.foo();
+		whatSayYou = helloWorld();
+		console.log({ whatSayYou });
 	});
 </script>
 
 <svelte:head>
 	<title>Rollup Plugin</title>
 </svelte:head>
+
+{#if whatSayYou}
+	<h1>{whatSayYou}</h1>
+{/if}
 
 <pre>
 	<code>{@html code}</code>
