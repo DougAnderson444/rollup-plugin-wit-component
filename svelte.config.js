f92bbb4d62f4bbc0a4c585096861be1b67f69de8
Doug A <douganderson444@gmail.com>
2023-06-20 08:59:15 -0300

    fix gh pages base path preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import 'dotenv/config';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +10,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			// default options are shown
+			pages: 'docs',
+			assets: 'docs',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
