112accf23b2ff29b1635fc4ac570ee2d08e17bdb
Doug Anderson444 <douganderson444@gmail.com>
2023-12-06 15:27:25 -0400

    add named export
    to test the export naming issue

--- crates/demo/src/lib.rs
+++ crates/demo/src/lib.rs
@@ -1,5 +1,6 @@
 cargo_component_bindings::generate!();
 
+use bindings::exports::component::cargo_comp::named;
 use bindings::Guest;
 
 struct Component;
@@ -9,5 +10,13 @@ impl Guest for Component {
     fn hello() -> String {
         bindings::component::cargo_comp::imports::prnt("Calling prnt from Rust");
         "Hello, World!".to_string()
+    }
+}
+
+impl named::Guest for Component {
+    /// Say hello!
+    fn name() -> String {
+        bindings::component::cargo_comp::imports::prnt("Calling prnt from Rust");
+        "There's a name here somewhere...".to_string()
     }
 }
