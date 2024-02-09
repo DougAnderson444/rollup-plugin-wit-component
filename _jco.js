e420c75b6daa6dc2d4a9d6112b4f4b607a32a4e9
Doug Anderson444 <douganderson444@gmail.com>
2024-02-09 10:04:07 -0400

    rm old jco deps

--- _jco.js
+++
@@ -1,3 +0,0 @@
-// uses a fork due to these issues: https://github.com/DougAnderson444/jco/tree/no-top-level-await
-// see https://github.com/bytecodealliance/jco/issues/284
-export { transpile } from "@bytecodealliance/jco";
